18ab504477a4b49a7a9e00727e82aed65524b7ff
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-05-18 13:13:59 -0500

    minor

--- src/orders/dto/orders.dto.ts
+++ src/orders/dto/orders.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { ValidateNested, IsArray } from 'class-validator';
-import { CreateOrderDto, SignedOBOrderDto } from './signed-ob-order.dto';
+import { CreateOrderDto } from './signed-ob-order.dto';
 
 export class OrdersDto {
   @ApiProperty({
     description: 'Orders to be saved',
-    type: [SignedOBOrderDto]
+    type: [CreateOrderDto]
   })
   @ValidateNested({ each: true, message: 'Invalid signed order' })
   @Type(() => CreateOrderDto)
