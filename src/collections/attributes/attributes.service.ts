dd0f9c8899b29db6b1e3c4e59fd90387952d8793
Sleeyax <yourd3veloper@gmail.com>
2022-06-04 13:57:23 +0200

    use constant

--- src/collections/attributes/attributes.service.ts
+++ src/collections/attributes/attributes.service.ts
@@ -1,11 +1,12 @@
 import { CollectionAttributes } from '@infinityxyz/lib/types/core';
+import { firestoreConstants } from '@infinityxyz/lib/utils/constants';
 import { Injectable } from '@nestjs/common';
 import { ParsedCollectionId } from 'collections/collection-id.pipe';
 
 @Injectable()
 export class AttributesService {
   async getAttributes(collection: ParsedCollectionId): Promise<CollectionAttributes> {
-    const snapshot = await collection.ref.collection('attributes').get(); // TODO: use COLLECTION_NFTS_ATTRIBUTES constant
+    const snapshot = await collection.ref.collection(firestoreConstants.COLLECTION_NFTS_ATTRIBUTES).get();
     const attributes: CollectionAttributes = {};
     snapshot.forEach((doc) => (attributes[doc.id] = doc.data() as any));
     return attributes;
