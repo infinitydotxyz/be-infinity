dedd85043e1e81ffca294e4a777fb9a7377883a0
ngduc <nducus@yahoo.com>
2022-07-01 12:08:40 -0700

    reverted

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { Multer } from 'multer';
 
 function setup(app: INestApplication) {
-  // todo: disable CORS for now for testing - enable this after prod release:
-  // app.enableCors({
-  //   origin: ORIGIN,
-  //   optionsSuccessStatus: 200
-  // });
+  app.enableCors({
+    origin: ORIGIN,
+    optionsSuccessStatus: 200
+  });
   app.use(helmet());
   app.useGlobalFilters(new HttpExceptionFilter());
 
