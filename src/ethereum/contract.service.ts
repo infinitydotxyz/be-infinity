c57ee102aeaf4a1c235df52a4878ba56eb25bcea
Sleeyax <yourd3veloper@gmail.com>
2022-07-09 16:29:07 +0200

    check whether chain id is supported

--- src/ethereum/contract.service.ts
+++ src/ethereum/contract.service.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@infinityxyz/lib/types/core';
 import { GOERLI_STAKER_CONTRACT_ADDRESS } from '@infinityxyz/lib/utils';
 import { InfinityStakerABI } from '@infinityxyz/lib/abi/infinityStaker';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { EthereumService } from './ethereum.service';
 
 @Injectable()
@@ -9,10 +9,15 @@ export class ContractService {
   constructor(private ethereumService: EthereumService) {}
 
   getStakerContract(chainId: string | ChainId) {
+    // TODO: return right contract based on specified chain id
+    if (chainId != ChainId.Goerli) {
+      throw new BadRequestException(`Chain id '${chainId}' is currently not supported!`);
+    }
+
     return this.ethereumService.getContract({
       abi: InfinityStakerABI,
       address: GOERLI_STAKER_CONTRACT_ADDRESS,
-      chainId: ChainId.Goerli // chainId
+      chainId: chainId
     });
   }
 }
