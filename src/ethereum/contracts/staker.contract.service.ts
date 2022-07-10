4d365c6edfd9b8ffa6cbfb22d43cedfb4e70cfe9
Sleeyax <yourd3veloper@gmail.com>
2022-07-10 16:13:53 +0200

    don't convert result of staking power number

--- src/ethereum/contracts/staker.contract.service.ts
+++ src/ethereum/contracts/staker.contract.service.ts
@@ -14,7 +14,7 @@ export class StakerContractService {
   async getPower(user: ParsedUserId): Promise<number> {
     const contract = this.contractService.getStakerContract(user.userChainId);
     const balance = await contract.getUserStakePower(user.userAddress);
-    return this.contractService.toEther(balance);
+    return balance;
   }
 
   /**
