1b2aef26023435a3102a90b3d543a637c694316f
nneverlander <findadit@gmail.com>
2022-07-11 12:11:49 +0200

    adds docs redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiFoundResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
+  @Get()
+  @Redirect('/docs/')
+  @ApiFoundResponse({
+    description: 'Redirect to /docs'
+  })
+  redirectToDocs() {
+    return;
+  }
+
   @Get('ping')
   @ApiOperation({
     description: 'Bare minimum sanity check'
