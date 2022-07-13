2f03b48b722145413635a89ac721bbc54f569890
nneverlander <findadit@gmail.com>
2022-07-13 10:26:05 +0200

    removes mnemonic from backfill service

--- src/zora/zora.service.ts
+++ src/zora/zora.service.ts
@@ -1,4 +1,4 @@
-import { ZoraAggregateCollectionStatsResponse } from '@infinityxyz/lib/types/services/zora';
+import { ZoraAggregateCollectionStatsResponse, ZoraTokensResponse } from '@infinityxyz/lib/types/services/zora';
 import { sleep } from '@infinityxyz/lib/utils';
 import { Injectable } from '@nestjs/common';
 import { ClientError, gql, GraphQLClient } from 'graphql-request';
@@ -72,6 +72,90 @@ export class ZoraService {
         }
       } else {
         console.error('failed to get aggregated collection stats info from zora', chainId, collectionAddress, e);
+      }
+    }
+  }
+
+  public async getTokens(
+    chainId: string,
+    collectionAddress: string,
+    after: string,
+    limit: number
+  ): Promise<ZoraTokensResponse | undefined> {
+    const numRetries = 3;
+    const data = await this.tryFetchTokens(chainId, collectionAddress, after, limit, numRetries);
+    return data as ZoraTokensResponse;
+  }
+
+  private async tryFetchTokens(
+    chainId: string,
+    collectionAddress: string,
+    after: string,
+    limit: number,
+    numRetries: number
+  ): Promise<ZoraTokensResponse | undefined> {
+    if (numRetries === 0) {
+      throw Error('Retries exceeded');
+    }
+
+    try {
+      const query = gql`
+        query MyQuery {
+          tokens(where: { collectionAddresses: "${collectionAddress}"}, networks: {network: ETHEREUM, chain: MAINNET}, pagination: {after: "${after}", limit: ${limit}}, sort: {sortKey: TOKEN_ID, sortDirection: ASC}) {
+            nodes {
+              token {
+                tokenId
+                tokenUrl
+                attributes {
+                  displayType
+                  traitType
+                  value
+                }
+                image {
+                  url
+                }
+                mintInfo {
+                  toAddress
+                  originatorAddress
+                  price {
+                    chainTokenPrice {
+                      decimal
+                      currency {
+                        address
+                        decimals
+                        name
+                      }
+                    }
+                  }
+                  mintContext {
+                    blockNumber
+                    transactionHash
+                    blockTimestamp
+                  }
+                }
+              }
+            }
+            pageInfo {
+              endCursor
+              hasNextPage
+              limit
+            }
+          }
+        }
+      `;
+
+      const data = await this.client.request(query);
+      return data as ZoraTokensResponse;
+    } catch (e) {
+      if (e instanceof ClientError) {
+        const status = e.response.status;
+        if (status === 429) {
+          // too many requests
+          await sleep(1000);
+          return this.tryFetchTokens(chainId, collectionAddress, after, limit, --numRetries);
+        }
+      } else {
+        console.error('Failed to get tokens from zora', chainId, collectionAddress, e);
       }
     }
   }
