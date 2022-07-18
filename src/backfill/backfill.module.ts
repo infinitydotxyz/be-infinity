377014b662d6d4cf1d4059ded376305607369c85
nneverlander <findadit@gmail.com>
2022-07-18 21:42:30 +0200

    removes mnemonic dependency in most places

--- src/backfill/backfill.module.ts
+++ src/backfill/backfill.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AlchemyModule } from 'alchemy/alchemy.module';
 import { GemModule } from 'gem/gem.module';
-import { MnemonicModule } from 'mnemonic/mnemonic.module';
 import { OpenseaModule } from 'opensea/opensea.module';
 import { BackfillService } from './backfill.service';
 
 @Module({
   providers: [BackfillService],
   exports: [BackfillService],
-  imports: [AlchemyModule, MnemonicModule, OpenseaModule, GemModule]
+  imports: [AlchemyModule, OpenseaModule, GemModule]
 })
 export class BackfillModule {}
