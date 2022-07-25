c871b44a44d6fd0cdbd1f2ac97865b979d9cc79c
nneverlander <findadit@gmail.com>
2022-07-25 12:42:56 +0200

    removes redis storage from main branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,32 @@
-import { StorageModule } from './storage/storage.module';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { BackfillModule } from 'backfill/backfill.module';
+import { CollectionsModule } from 'collections/collections.module';
+import { FeedModule } from 'feed/feed.module';
+import { GemModule } from 'gem/gem.module';
 import { LoggerMiddleware } from 'logger.middleware';
+import { MnemonicModule } from 'mnemonic/mnemonic.module';
+import { OpenseaModule } from 'opensea/opensea.module';
+import { OrdersModule } from 'orders/orders.module';
+import { join } from 'path';
+import { ReservoirModule } from 'reservoir/reservoir.module';
+import { ApiKeyThrottlerGuard } from 'throttler/throttler.guard';
+import { ZoraModule } from 'zora/zora.module';
+import { AlchemyModule } from './alchemy/alchemy.module';
+import { ApiUserModule } from './api-user/api-user.module';
 import { AppController } from './app.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { FB_STORAGE_BUCKET } from './constants';
+import { DiscordModule } from './discord/discord.module';
+import { EthereumModule } from './ethereum/ethereum.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { StatsModule } from './stats/stats.module';
-import { join } from 'path';
+import { StorageModule } from './storage/storage.module';
 import { TwitterModule } from './twitter/twitter.module';
-import { DiscordModule } from './discord/discord.module';
 import { UserModule } from './user/user.module';
-import { CollectionsModule } from 'collections/collections.module';
-import { OrdersModule } from 'orders/orders.module';
-import { MnemonicModule } from 'mnemonic/mnemonic.module';
-import { FB_STORAGE_BUCKET } from './constants';
-import { AlchemyModule } from './alchemy/alchemy.module';
-import { EthereumModule } from './ethereum/ethereum.module';
-import { BackfillModule } from 'backfill/backfill.module';
-import { ZoraModule } from 'zora/zora.module';
-import { OpenseaModule } from 'opensea/opensea.module';
-import { ReservoirModule } from 'reservoir/reservoir.module';
-import { GemModule } from 'gem/gem.module';
-import { APP_GUARD } from '@nestjs/core';
-import { ApiKeyThrottlerGuard } from 'throttler/throttler.guard';
-import { ApiUserModule } from './api-user/api-user.module';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { FeedModule } from 'feed/feed.module';
 
 import * as serviceAccount from './creds/nftc-dev-firebase-creds.json';
-import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 
 @Module({
   imports: [
@@ -55,27 +54,11 @@ import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
     OpenseaModule,
     ReservoirModule,
     GemModule,
-    ThrottlerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const redisUrl = config.get<string>('REDIS_URL');
-        let storage = undefined;
-        if (redisUrl) {
-          storage = new ThrottlerStorageRedisService(redisUrl);
-        }
-        return {
-          ttl: 60,
-          limit: 10,
-          storage
-        };
-      }
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+      storage: undefined
     }),
-    // ThrottlerModule.forRoot({
-    //   ttl: 60,
-    //   limit: 10,
-    //   storage: process.env.REDIS_URL ? new ThrottlerStorageRedisService(process.env.REDIS_URL) : undefined
-    // }),
     ApiUserModule
   ],
   providers: [
