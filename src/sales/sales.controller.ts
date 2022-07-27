e69c3d8f0a927ea2b3a5d98710629de904a9c1c8
nneverlander <findadit@gmail.com>
2022-07-27 17:43:45 +0200

    moves sales types to lib

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -4,7 +4,7 @@ import { ApiTag } from 'common/api-tags';
 import { ErrorResponseDto } from 'common/dto/error-response.dto';
 import { ResponseDescription } from 'common/response-description';
 import SalesService from './sales.service';
-import { InfinitySalesQueryDto, SalesResponse } from './sales.types';
+import { InfinitySalesQueryDto, NftSalesResponseDto } from '@infinityxyz/lib/types/dto/sales';
 
 @Controller('sales')
 export class SalesController {
@@ -15,10 +15,10 @@ export class SalesController {
     description: 'Fetches Infinity sales',
     tags: [ApiTag.Sales]
   })
-  @ApiOkResponse({ description: ResponseDescription.Success, type: SalesResponse })
+  @ApiOkResponse({ description: ResponseDescription.Success, type: NftSalesResponseDto })
   @ApiBadRequestResponse({ description: ResponseDescription.BadRequest, type: ErrorResponseDto })
   @ApiInternalServerErrorResponse({ description: ResponseDescription.InternalServerError })
-  public async getInfinitySales(@Query() reqQuery: InfinitySalesQueryDto): Promise<SalesResponse> {
+  public async getInfinitySales(@Query() reqQuery: InfinitySalesQueryDto): Promise<NftSalesResponseDto> {
     const results = await this.salesService.getInfinitySales(reqQuery.cursor ?? '', reqQuery.limit ?? 100);
     return results;
   }
--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,15 +1,15 @@
 import { NftSale, SaleSource } from '@infinityxyz/lib/types/core';
+import { NftSalesResponseDto } from '@infinityxyz/lib/types/dto/sales';
 import { firestoreConstants } from '@infinityxyz/lib/utils';
 import { Injectable } from '@nestjs/common';
 import { FirebaseService } from '../firebase/firebase.service';
 import { CursorService } from '../pagination/cursor.service';
-import { SalesResponse } from './sales.types';
 
 @Injectable()
 export default class SalesService {
   constructor(private firebaseService: FirebaseService, private cursorService: CursorService) {}
 
-  public async getInfinitySales(cursor: string, limit: number): Promise<SalesResponse> {
+  public async getInfinitySales(cursor: string, limit: number): Promise<NftSalesResponseDto> {
     try {
       const salesCollectionRef = this.firebaseService.firestore.collection(firestoreConstants.SALES_COLL);
       let timestampCursor = this.cursorService.decodeCursorToNumber(cursor);
