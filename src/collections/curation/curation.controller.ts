ce9fcb07b213044cb413559b53396cd6027b08fc
Sleeyax <yourd3veloper@gmail.com>
2022-08-05 14:14:24 +0200

    Add vote in bulk endpoint

--- src/collections/curation/curation.controller.ts
+++ src/collections/curation/curation.controller.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, ParseArrayPipe, Post } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiBody,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
@@ -14,11 +15,15 @@ import { ResponseDescription } from 'common/response-description';
 import { ParseUserIdPipe } from 'user/parser/parse-user-id.pipe';
 import { ParsedUserId } from 'user/parser/parsed-user-id';
 import { CurationService } from './curation.service';
-import { CurationVoteDto } from '@infinityxyz/lib/types/dto/collections/curation/curation-vote.dto';
+import {
+  CurationVoteBulkDto,
+  CurationVoteDto
+} from '@infinityxyz/lib/types/dto/collections/curation/curation-vote.dto';
 import { Auth } from 'auth/api-auth.decorator';
 import { SiteRole } from 'auth/auth.constants';
 import { ParamUserId } from 'auth/param-user-id.decorator';
 import { ApiRole } from '@infinityxyz/lib/types/core/api-user';
+import { ParsedBulkVotes, ParsedBulkVotesPipe } from './bulk-votes.pipe';
 
 @Controller('collections')
 export class CurationController {
@@ -52,5 +57,30 @@ export class CurationController {
       user,
       votes: vote.votes
     });
+  }
+
+  @Post('curated/:userId')
+  @Auth(SiteRole.User, ApiRole.Guest, 'userId')
+  @ApiOperation({
+    description: 'Vote on multiple collections in bulk',
+    tags: [ApiTag.Collection, ApiTag.Curation]
+  })
+  @ApiBody({ type: CurationVoteBulkDto, isArray: true })
+  @ApiCreatedResponse({ description: ResponseDescription.Success })
+  @ApiBadRequestResponse({ description: ResponseDescription.BadRequest, type: ErrorResponseDto })
+  @ApiNotFoundResponse({ description: ResponseDescription.NotFound, type: ErrorResponseDto })
+  @ApiInternalServerErrorResponse({ description: ResponseDescription.InternalServerError, type: ErrorResponseDto })
+  async voteBulk(
+    @ParamUserId('userId', ParseUserIdPipe) user: ParsedUserId,
+    @Body(new ParseArrayPipe({ items: CurationVoteBulkDto }), ParsedBulkVotesPipe) votes: ParsedBulkVotes[]
+  ) {
+    const totalVotesToSpend = votes.map((v) => v.votes).reduce((x: number, y: number) => x + y, 0);
+    const availableVotes = await this.curationService.getAvailableVotes(user);
+
+    if (availableVotes <= 0 || availableVotes < totalVotesToSpend) {
+      throw new BadRequestException('Insufficient amount of votes available');
+    }
+
+    return this.curationService.voteBulk(votes, user);
   }
 }
--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,6 +1,9 @@
+import { ChainId } from '@infinityxyz/lib/types/core';
 import { Collection } from '@infinityxyz/lib/types/core/Collection';
-import { firestoreConstants, getCollectionDocId } from '@infinityxyz/lib/utils';
+import { firestoreConstants, getCollectionDocId, trimLowerCase } from '@infinityxyz/lib/utils';
 import { BadRequestException, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ParsedCollectionId } from 'collections/collection-id.pipe';
+import { ethers } from 'ethers';
 import firebaseAdmin, { storage } from 'firebase-admin';
 import { CollectionRefDto } from './dto/collection-ref.dto';
 import { FIREBASE_OPTIONS } from './firebase.constants';
@@ -61,5 +64,44 @@ export class FirebaseService {
     } else {
       throw new BadRequestException(`Failed to provide a collection slug or address`);
     }
+  }
+
+  async parseCollectionId(value: string): Promise<ParsedCollectionId> {
+    const [chainIdOrSlug, address] = value.split(':').map((item) => trimLowerCase(item));
+    let chainId, slug;
+    let collectionRef: FirebaseFirestore.DocumentReference<Collection>;
+    if (address) {
+      chainId = chainIdOrSlug;
+      collectionRef = (await this.getCollectionRef({
+        chainId: chainId as ChainId,
+        address
+      })) as FirebaseFirestore.DocumentReference<Collection>;
+    } else {
+      slug = chainIdOrSlug;
+
+      if (!slug) {
+        throw new BadRequestException('Invalid slug');
+      }
+
+      collectionRef = (await this.getCollectionRef({
+        slug
+      })) as FirebaseFirestore.DocumentReference<Collection>;
+    }
+
+    const [chainIdFromRef, addressFromRef] = collectionRef.id.split(':');
+
+    if (!Object.values(ChainId).includes(chainIdFromRef as any)) {
+      throw new BadRequestException('Invalid chain id');
+    }
+
+    if (!ethers.utils.isAddress(addressFromRef)) {
+      throw new BadRequestException('Invalid address');
+    }
+
+    return {
+      address: addressFromRef,
+      chainId: chainIdFromRef as ChainId,
+      ref: collectionRef
+    };
   }
 }
