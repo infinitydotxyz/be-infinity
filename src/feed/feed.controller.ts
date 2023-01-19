b3f45a2bf39239136d434d63e6bbe5b0495480eb
nneverlander <findadit@gmail.com>
2023-01-19 11:29:15 -0300

    removes incrementLike endpoint from feed controller

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,42 +1,19 @@
-import { ApiRole } from '@infinityxyz/lib/types/core/api-user';
-import { Body, Controller, Post } from '@nestjs/common';
+import { NftActivityArrayDto, NftActivityFiltersDto } from '@infinityxyz/lib/types/dto/collections/nfts';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
-  ApiInternalServerErrorResponse,
-  ApiNotFoundResponse,
-  ApiOkResponse,
+  ApiInternalServerErrorResponse, ApiOkResponse,
   ApiOperation
 } from '@nestjs/swagger';
-import { Auth } from 'auth/api-auth.decorator';
-import { SiteRole } from 'auth/auth.constants';
 import { ApiTag } from 'common/api-tags';
 import { ErrorResponseDto } from 'common/dto/error-response.dto';
+import { CacheControlInterceptor } from 'common/interceptors/cache-control.interceptor';
 import { ResponseDescription } from 'common/response-description';
 import { FeedService } from './feed.service';
-import { IncrementQuery } from './feed.types';
-import { Get, Query, UseInterceptors } from '@nestjs/common';
-import { NftActivityArrayDto, NftActivityFiltersDto } from '@infinityxyz/lib/types/dto/collections/nfts';
-import { CacheControlInterceptor } from 'common/interceptors/cache-control.interceptor';
 
 @Controller('feed')
 export class FeedController {
   constructor(private feedService: FeedService) {}
-
-  @Post('/:userId/like')
-  @Auth(SiteRole.User, ApiRole.Guest, 'userId')
-  @ApiOperation({
-    description: 'Increment likes',
-    tags: [ApiTag.Feed]
-  })
-  @ApiOkResponse({ type: Boolean })
-  @ApiBadRequestResponse({ description: ResponseDescription.BadRequest, type: ErrorResponseDto })
-  @ApiNotFoundResponse({ description: ResponseDescription.NotFound, type: ErrorResponseDto })
-  @ApiInternalServerErrorResponse({ description: ResponseDescription.InternalServerError, type: ErrorResponseDto })
-  public async incrementLikes(@Body() payload: IncrementQuery) {
-    await this.feedService.incrementLikes(payload);
-
-    return true;
-  }
 
   @Get('activity')
   @ApiOperation({
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,46 +1,12 @@
 import { NftActivityFiltersDto } from '@infinityxyz/lib/types/dto/collections/nfts';
-import { firestoreConstants } from '@infinityxyz/lib/utils';
 import { Injectable } from '@nestjs/common';
 import { FirebaseService } from 'firebase/firebase.service';
-import { FirestoreDistributedCounter } from 'firebase/firestore-counter';
 import { CursorService } from 'pagination/cursor.service';
 import { getNftActivity } from 'utils/activity';
-import { IncrementQuery, LikedLock } from './feed.types';
 
 @Injectable()
 export class FeedService {
   constructor(private firebaseService: FirebaseService, private paginationService: CursorService) {}
-
-  public async incrementLikes(query: IncrementQuery) {
-    const likeLockRef = this.firebaseService.firestore
-      .collection(firestoreConstants.FEED_COLL)
-      .doc(query.eventId)
-      .collection('likeLocks')
-      .doc(query.userAddress);
-
-    const likeLock = await likeLockRef.get();
-    const lockData = likeLock.data() as LikedLock;
-
-    if (!lockData) {
-      await likeLockRef.set({ liked: query.liked } as LikedLock);
-
-      this.incrementLikesCounter(query.liked ? 1 : -1, query.eventId);
-    } else {
-      if (lockData.liked !== query.liked) {
-        await likeLockRef.set({ liked: query.liked } as LikedLock);
-
-        this.incrementLikesCounter(query.liked ? 2 : -2, query.eventId);
-      }
-    }
-  }
-
-  private incrementLikesCounter(increment: number, docId: string) {
-    const docRef = this.firebaseService.firestore.collection(firestoreConstants.FEED_COLL).doc(docId);
-
-    const numLikes = new FirestoreDistributedCounter(docRef, 'likes');
-
-    numLikes.incrementBy(increment);
-  }
 
   async getActivity(filter: NftActivityFiltersDto) {
     const eventTypes = typeof filter.eventType === 'string' ? [filter.eventType] : filter.eventType;
