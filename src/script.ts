4f43de2e4da86cf32efe602e6b818316bbc22331
nneverlander <findadit@gmail.com>
2023-06-08 19:44:40 -0500

    minor

--- src/script.ts
+++ src/script.ts
@@ -122,7 +122,7 @@ export const run = async () => {
 
   // await calcTotalBuyRewards();
 
-  const timestamps = [1685750400000];
+  const timestamps = [1686182400000];
   for (const timestamp of timestamps) {
     await calcDailyBuyRewards(timestamp);
   }
