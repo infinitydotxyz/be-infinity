bade4436ea65c275952640452f9f2a50a2ea27fe
nneverlander <findadit@gmail.com>
2023-07-05 16:30:23 -0500

    simplified current user rewards

---
+++ src/rewards/types.ts
@@ -0,0 +1,8 @@
+export interface UserXFLRewards {
+  chainId: string;
+  address: string;
+  airdropRewardAmount: number;
+  buyRewardAmount: number;
+  referralRewardAmount: number;
+  totalRewardAmount: number;
+}
